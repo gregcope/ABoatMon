460f7aa2e8e8aa9c27e37f92b1f55ac5315b0430
Greg Cope <gregcope@gmail.com>
2016-05-03 21:48:28 +0100

    Added LED, ChargerPin and GPS setup

--- ABoatMon.ino
+++ ABoatMon.ino
@@ -7,16 +7,41 @@
 // PIN defines
 #define GPS_POWER 0
 
-// objects
+#define BUTTON_LED 13
+#define CHARGER_POWER 14
+
+// debug functions
+
+#define DEBUG(input)   {Serial.print(input); Serial.flush();}
+#define DEBUGln(input) {Serial.println(input); Serial.flush();}
+
+// Objects
 TinyGPSPlus gps;
-Device GPSDevice(GPS_POWER); 
+Device gpsDevice(GPS_POWER); 
+Device charger(CHARGER_POWER);
+Device led(BUTTON_LED);
 
 void setup() {
   // put your setup code here, to run once:
-
+  setupGPS();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void setupGPS() {
+
+  // power up and configure GPS device
+  gpsDevice.on();
+  Serial1.begin(9600); 
+  // http://forums.adafruit.com/viewtopic.php?f=19&p=143502
+  Serial1.print("$PMTK313,1*2E\r\n");  // Enable to search a SBAS satellite
+  Serial1.print("$PMTK301,2*2E\r\n");  // Enable WAAS as DGPS Source
+  // https://code.google.com/p/ardupilot-mega/source/browse/libraries/AP_GPS/AP_GPS_NMEA.h?spec=svneff9f9d0906c1e3cd4e217b0363d0f9d44394e75&name=f401de6d52&r=eff9f9d0906c1e3cd4e217b0363d0f9d44394e75
+  Serial1.print("$PMTK220,1000*1F\r\n"); // 1HZ
+  //Serial1.print("$PSRF151,1*3F\r\n"); // WAAS_ON
+  //Serial1.print("$PMTK513,1*28\r\n"); // Search for SBAS Sat
+  DEBUGln("setupGPS");
 }
--- Device.h
+++ Device.h
@@ -5,6 +5,8 @@
 // Device
 //
 // Library for a Device, that has on on/off state
+// Aimed to be used for LEDs, GPS etc.. 
+// Anything that has a PIN set to OUTPUT and set to HIGH for on
 //
 // Greg Cope <greg.cope@gmail.com>
 // 04-May-2016 Initial version
