289b8688ef107233a43b386282fd5081462af0ce
Greg Cope <gregcope@gmail.com>
2016-05-03 21:15:18 +0100

    Ops forgot this one\!

---
+++ ABoatMon.ino
@@ -0,0 +1,22 @@
+
+
+// includes
+#include "TinyGPS++.h"
+#include "Device.h"
+
+// PIN defines
+#define GPS_POWER 0
+
+// objects
+TinyGPSPlus gps;
+Device GPSDevice(GPS_POWER); 
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
