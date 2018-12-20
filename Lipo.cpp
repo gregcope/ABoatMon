aa4fafee0788eaedb03c8f027b6b3c00fb4cbe69
Greg Cope <gregcope@gmail.com>
2018-12-20 15:03:02 +0000

    updated lipo code

--- Lipo.cpp
+++ Lipo.cpp
@@ -15,12 +15,16 @@ float Lipo::read(void) {
   analogRead(_voltageDividerPin);
     for (byte i=0; i<10; i++) {
       //take 10 samples, and average
-      DEBUG("analogRead: ");
-      DEBUGln(analogRead(_voltageDividerPin));
+      //DEBUG("analogRead: ");
+      //DEBUGln(analogRead(_voltageDividerPin));
       lipoBatteryReadings+=analogRead(_voltageDividerPin);
     }
 
   lipoBatteryVolts = BATT_FORMULA(lipoBatteryReadings / 10.0);
-  DEBUGln(lipoBatteryVolts);
+  //DEBUGln(lipoBatteryVolts);
   return(lipoBatteryVolts);
+
+  //  dtostrf(batteryVolts,3,2, BATstr); //update the BATStr which gets sent every BATT_CYCLES or along with the MOTION message
+  // 3 chars long 2 after decimal point
+  //if (batteryVolts <= BATT_LOW) BATstr = "LOW";
 }
--- Lipo.h
+++ Lipo.h
@@ -15,11 +15,13 @@
 #define DEBUG(input)   {Serial.print(input); Serial.flush();}
 #define DEBUGln(input) {Serial.println(input); Serial.flush();}
 
-// 3.3 (Vcc) / 1024 (ADC precision) =  0.00322
+// 3.3 (Vcc) / 1024 (ADC precision) =  0.0032258
 // Motineo example 1m+470k = 470 / ( 1000 +470 ) = 0.32 rounded up
-// Ours 1.8m + 3.3m = 3300000 / ( 1800000 + 3300000 ) = 0.647 or 1.55 (inverse)
-#define BATT_FORMULA(reading) reading * 0.00322 * 1.5455 // >>> fine tune this parameter to match your voltage when fully charged
+// Ours 1.8m + 3.3m = 3300000 / ( 1800000 + 3300000 ) = 0.64705 or 1.55 (inverse)
+//#define BATT_FORMULA(reading) reading * 0.0032258 * 1.5455 // >>> fine tune this parameter to match your voltage when fully charged
                                                        // details on how this works: https://lowpowerlab.com/forum/index.php/topic,1206.0.html
+// measured                                                
+#define BATT_FORMULA(reading) reading * 0.004782608696
 
 class Lipo
 {
