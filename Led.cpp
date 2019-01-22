f27a622ff66077a824ee994ccc38388c9ee4943b
Greg Cope <greg.cope@ft.com>
2019-01-22 17:35:47 +0000

    Comments update for Led.cpp

--- Led.cpp
+++ Led.cpp
@@ -9,20 +9,27 @@ Led::Led(int pin) {
 }
 
 void Led::on(void) {
+
+  // switch if on and return (with it on)
   digitalWrite(_ledPin,HIGH);
 }
 
 void Led::off(void) {
+
+  // simply switch it off and return (with it off)
   digitalWrite(_ledPin,LOW); 
 }
 
-void Led::blink(int DELAY_MS, byte loops)
-{
+void Led::blink(int DELAY_MS, byte loops) {
+
+  // Blinks an LED ... blocking
+  // for DELAY_MS
+  // and loops number of times
+
   pinMode(_ledPin, OUTPUT);
-  while (loops--)
-  {
-    digitalWrite(_ledPin,HIGH);
+  while (loops--) {
+    digitalWrite(_ledPin, HIGH);
     delay(DELAY_MS);
-    digitalWrite(_ledPin,LOW);
+    digitalWrite(_ledPin, LOW);
   }
 }
