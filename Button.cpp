8a7a3d21bf1ec6244f27b22c76d0a70a7205db3e
Greg Cope <greg.cope@ft.com>
2019-01-24 09:19:04 +0000

    Button comments

--- Button.cpp
+++ Button.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include "Button.h"
 
+// Not much too this ...
+
 Button::Button(byte pin) {
   // Constructor
-  // Takes an int as pin to power up
+  // Takes an int as pin to waatch
   pinMode (_buttonPin, INPUT_PULLUP);
 }
 
