b484a59274098bf13b9d94dad9bb248da7ce572f
Greg Cope <gregcope@gmail.com>
2019-02-03 13:48:33 +0000

    added logic and add missing line

--- Sleep.cpp
+++ Sleep.cpp
@@ -3,7 +3,27 @@
 
 Sleep::Sleep() {
   // Constructor
-  _now = millis();
+  // when we were made
+  _now = 0;
+  _time = 0;
+
+
+  // 1st time
+  // time = 0, now = 0
+  // having run 4200 millis since boot
+  // time = 0 + 8000 + 4200 - 0
+  // after 8 sec sleep,
+  // millis = 4200 (does not increase on sleep)
+  // time == 12200 
+  // wake now = 4200 (as we capture millis() on wake)
+
+  // 2nd time
+  // assume 1 second run time
+  // millis = 5200
+  // time = 12200 + 8000 + 5200 - 4200
+  // time = 21200
+  // time should be ... 12.2 secs + 1 sec + 8 secs = 21200 millis
+  // correct !!!
 }
 
 uint32_t Sleep::time(void){
@@ -32,6 +52,8 @@ void Sleep::kip2Secs(void) {
   // set time before kipping
   _time = _time + 2000 + millis() - _now;
   LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
+   // woke up and track millis now;
+  _now = millis();
   Serial.flush();
   Serial1.flush();
 }
